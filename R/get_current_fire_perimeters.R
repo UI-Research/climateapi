b08c20ac6995f8be96c1f500601f15c82a3104fb
William Curran-groome <wcurrangroome@williams-air.mynetworksettings.com>
2025-01-19 14:01:53 -0500

    first major commit

---
+++ R/get_current_fire_perimeters.R
@@ -0,0 +1,67 @@
+# Author: Will Curran-Groome
+
+#' @importFrom magrittr %>%
+
+#' @title Acquire wildfire perimeters
+#' @param geography Included only for API consistency; this must be NULL.
+#' @param file_path Included only for API consistency; this must be NULL.
+#' @param bbox Optionally, an sf::st_bbox() object, or an object that can be converted to such.
+#' @param api Included only for API consistency; this must be TRUE.
+#'
+#' @returns A library(sf) enabled dataframe comprising perimeters of current wildfires.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_current_fire_perimeters()
+#' }
+get_current_fire_perimeters = function(
+  geography = NULL,
+  file_path = NULL,
+  bbox = NULL,
+  api = TRUE) {
+
+  ## if specified, is `bbox` a valid sf::st_bbox() object?
+  if (!is.null(bbox)) {
+    bbox = tryCatch(
+      expr = { sf::st_bbox(bbox) },
+      error = function(e) {
+        warning(e)
+        stop("Please specify a valid bbox object using sf::st_bbox(), or at minimum,
+             an object that can be converted to such.")})}
+
+  ## is a valid geography option provided? (must be NULL)
+  if (!is.null(geography)) {
+    stop("Invalid geography. Geography must be NULL.")}
+
+  ## data may only be queried from the API
+  if (!isTRUE(api)) {
+    stop("Data must be queried from the API; set `api = TRUE`.")}
+
+  url_perimeters = "https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/WFIGS_Interagency_Perimeters_Current/FeatureServer/0"
+
+  df_perimeters1 = esri2sf::esri2sf(url = url, bbox = bbox) %>%
+    janitor::clean_names() %>%
+    dplyr::transmute(
+      unique_id = uuid::UUIDgenerate(n = nrow(.)),
+      incident_name = attr_incident_name %>% stringr::str_to_title,
+      incident_size_acres = attr_incident_size,
+      incident_short_description = attr_incident_short_description,
+      percent_contained = attr_percent_contained,
+      identified_date = attr_fire_discovery_date_time %>% lubridate::as_datetime,
+      updated_date = attr_modified_on_date_time_dt %>% lubridate::as_datetime)
+
+  message(stringr::str_c(
+    "Each observation represents a distinct wildfire and its perimeters. ",
+    "Observations returned are those that intersect with the argument supplied ",
+    "to `bbox`. If no argument was supplied, observations returned reflect all current ",
+    "wildfires in the US. ",
+    "The `unique_id` field is a unique identifier for each observation. "))
+
+  return(df_perimeters1)
+}
+
+utils::globalVariables(c(
+  "attr_incident_name", "attr_incident_size", "attr_incident_short_description",
+  "attr_percent_contained", "attr_fire_discovery_date_time", ".",
+  "attr_modified_on_date_time_dt"))
---
+++ R/get_fema_disaster_declarations_county.R
@@ -0,0 +1,115 @@
+# Author: Kameron Lloyd
+
+#' @importFrom magrittr %>%
+
+#' @title Get Major Disaster Declarations by County
+#' @param geography Included for API consistency only; must be `county`.
+#' @param file_path The path (on Box) to the file containing the raw data.
+#' @param api If TRUE (default), access data from the API. Else, read locally from `file_path`.
+#' @returns A dataframe comprising Major Disaster Declarations by month by year by county.
+#' @export
+#' @examples
+#' \dontrun{
+#' get_fema_disaster_declarations_county(
+#'   api = TRUE)
+#' }
+get_fema_disaster_declarations_county = function(
+	geography = "county",
+	file_path = file.path(
+	  path.expand("~"), "Box", "METRO Climate and Communities Practice Area",
+	  "github-repository", "hazards", "fema", "disaster-declarations",
+		"raw", "fema_disaster_declarations_county_2024_10_25.csv"),
+	api = TRUE) {
+
+	## is a valid geography option provided?
+	if (!geography == "county") {
+		stop("Invalid geography. Valid options are 'county'.")}
+
+	## is the file path valid/accessible?
+	if (!file.exists(file_path) & !api) {
+		stop(stringr::str_c(
+			"The path to the dataset does not point to a valid file. ",
+			"Please ensure there is a file located at this path: ", file_path, ".")) }
+
+	if (!api) {
+		disaster_declarations1 <- readr::read_csv(file_path) %>%
+			janitor::clean_names()
+	} else {
+		disaster_declarations1 = rfema::open_fema(
+				data_set = "DisasterDeclarationsSummaries",
+				# Major Disaster Declarations Only
+				filters = list(declarationType = "=DR"),
+				ask_before_call = FALSE) %>%
+			janitor::clean_names() }
+
+	# Defining natural hazards so we can filter out disaster declarations for things like terrorist attacks/biological
+	natural_hazards <- c(
+		"Fire", "Flood", "Hurricane", "Severe Storm", "Winter Storm", "Tornado", "Snowstorm",
+		"Earthquake", "Mud/Landslide", "Coastal Storm", "Severe Ice Storm",
+		"Tropical Storm", "Typhoon", "Volcanic Eruption", "Tsunami", "Freezing", "Drought") %>%
+		paste0("incidents_", .) %>%
+		janitor::make_clean_names()
+
+	## the data date back multiple decade, so there are some valid observations for counties that
+	## no longer exist as of 2010 or 2022; we drop those counties
+	## counties that exist in 2010 or 2022
+	benchmark_geographies = dplyr::bind_rows(
+	  tigris::counties(cb = TRUE, year = 2010) %>%
+	    dplyr::transmute(GEOID = stringr::str_c(STATE, COUNTY)) %>%
+	    sf::st_drop_geometry(),
+	  tigris::counties(cb = TRUE, year = 2022) %>%
+	    dplyr::select(GEOID) %>%
+	    sf::st_drop_geometry()) %>%
+	  dplyr::pull(GEOID) %>%
+	  unique()
+
+	# Counting disaster declarations by county by year and month
+	disaster_declarations <- disaster_declarations1 %>%
+		dplyr::mutate(
+			fips_state_code = stringr::str_pad(as.character(fips_state_code), width = 2, side = "left", pad = "0"),
+			fips_county_code = stringr::str_pad(as.character(fips_county_code), width = 3, side = "left", pad = "0"),
+			GEOID = stringr::str_c(fips_state_code, fips_county_code),
+			year_declared = lubridate::year(declaration_date),
+			month_declared = lubridate::month(declaration_date)) %>%
+		## Major Disaster Declarations only
+		dplyr::filter(declaration_type == "DR") %>%
+		## produce counts at the county x incident-type x year x month level
+		dplyr::group_by(GEOID, incident_type, year_declared, month_declared) %>%
+			dplyr::summarise(count = dplyr::n()) %>%
+			dplyr::ungroup() %>%
+	  dplyr::arrange(GEOID, year_declared, month_declared) %>%
+		## widen the data so that there is one row per county x year x month, with
+		## incident-level counts in columns
+		tidyr::pivot_wider(names_from = incident_type, values_from = count, values_fill = list(count = 0), names_prefix = "incidents_") %>%
+	  dplyr::rename_with(.cols = dplyr::matches("incidents_"), janitor::make_clean_names) %>%
+	  dplyr::mutate(
+			unique_id = uuid::UUIDgenerate(n = nrow(.)),
+			incidents_all = rowSums(dplyr::select(., dplyr::matches("incidents")), na.rm = TRUE),
+			incidents_natural_hazard = rowSums(dplyr::select(., dplyr::all_of(natural_hazards)), na.rm = TRUE)) %>%
+	  dplyr::rename_with(.cols = -GEOID, .fn = ~ .x %>% stringr::str_to_lower()) %>%
+	  dplyr::select(
+			unique_id,
+			GEOID,
+			year_declared,
+			month_declared,
+			incidents_all,
+			incidents_natural_hazard,
+			dplyr::everything()) %>%
+	  dplyr::arrange(
+			GEOID,
+			year_declared,
+			month_declared) %>%
+	  dplyr::filter(GEOID %in% benchmark_geographies)
+
+	message(stringr::str_c(
+"The unit of observation is: ", geography, " x year x month. ",
+"The `unique_id` field is a unique identifier for each observation. ",
+"Note that these data only describe Major Disaster Declarations."))
+
+	return(disaster_declarations)
+}
+
+utils::globalVariables(c(
+  "get_box_data_path", "fips_state_code", "fips_county_code", "declaration_date",
+  "declaration_type", "incident_type", "GEOID", "year_declared", "month_declared",
+  ".", "n", "count", "incidents_all", "incidents_natural_hazard", "benchmark_geographies"))
