cd897391858153b5e477332bf2773b20f7b56380
wcurrangroome <wcurrangroome@gmail.com>
2025-06-27 16:31:25 -0400

    updating file names and documentation

--- R/get_fema_disaster_declarations.R
+++ R/get_fema_disaster_declarations.R
@@ -1,28 +1,19 @@
 # Author: Kameron Lloyd
 
-#' @importFrom magrittr %>%
-
 #' @title Get major disaster declarations by county
-#' @param geography Included for API consistency only; must be `county`.
 #' @param file_path The path (on Box) to the file containing the raw data.
 #' @param api If TRUE (default), access data from the API. Else, read locally from `file_path`.
 #' @returns A dataframe comprising Major Disaster Declarations by month by year by county.
 #' @export
 #' @examples
 #' \dontrun{
-#' get_fema_disaster_declarations_county(
-#'   api = TRUE)
+#' get_fema_disaster_declarations(api = TRUE)
 #' }
-get_fema_disaster_declarations_county = function(
-	geography = "county",
+get_fema_disaster_declarations = function(
 	file_path = file.path(
 	  get_box_path(), "hazards", "fema", "disaster-declarations",
 		"raw", "fema_disaster_declarations_county_2024_10_25.csv"),
 	api = TRUE) {
-
-	## is a valid geography option provided?
-	if (!geography == "county") {
-		stop("Invalid geography. Valid options are 'county'.")}
 
 	## is the file path valid/accessible?
 	if (!file.exists(file_path) & !api) {
@@ -31,63 +22,63 @@ get_fema_disaster_declarations_county = function(
 			"Please ensure there is a file located at this path: ", file_path, ".")) }
 
 	if (!api) {
-		disaster_declarations1 <- readr::read_csv(file_path) %>%
+		disaster_declarations1 <- readr::read_csv(file_path) |>
 			janitor::clean_names()
 	} else {
 		disaster_declarations1 = rfema::open_fema(
 				data_set = "DisasterDeclarationsSummaries",
 				# Major Disaster Declarations Only
 				filters = list(declarationType = "=DR"),
-				ask_before_call = FALSE) %>%
+				ask_before_call = FALSE) |>
 			janitor::clean_names() }
 
 	# Defining natural hazards so we can filter out disaster declarations for things like terrorist attacks/biological
 	natural_hazards <- c(
 		"Fire", "Flood", "Hurricane", "Severe Storm", "Winter Storm", "Tornado", "Snowstorm",
 		"Earthquake", "Mud/Landslide", "Coastal Storm", "Severe Ice Storm",
-		"Tropical Storm", "Typhoon", "Volcanic Eruption", "Tsunami", "Freezing", "Drought") %>%
-		paste0("incidents_", .) %>%
+		"Tropical Storm", "Typhoon", "Volcanic Eruption", "Tsunami", "Freezing", "Drought") |>
+		paste0("incidents_", .) |>
 		janitor::make_clean_names()
 
 	## the data date back multiple decades, so there are some valid observations for counties that
 	## no longer exist as of 2010 or 2022; we drop those counties
 	## counties that exist in 2010 or 2022
 	benchmark_geographies = dplyr::bind_rows(
-	  tigris::counties(cb = TRUE, year = 2010, progress_bar = FALSE) %>%
-	    dplyr::transmute(GEOID = stringr::str_c(STATE, COUNTY)) %>%
+	  tigris::counties(cb = TRUE, year = 2010, progress_bar = FALSE) |>
+	    dplyr::transmute(GEOID = stringr::str_c(STATE, COUNTY)) |>
 	    sf::st_drop_geometry(),
-	  tigris::counties(cb = TRUE, year = 2022, progress_bar = FALSE) %>%
-	    dplyr::select(GEOID) %>%
-	    sf::st_drop_geometry()) %>%
-	  dplyr::pull(GEOID) %>%
+	  tigris::counties(cb = TRUE, year = 2022, progress_bar = FALSE) |>
+	    dplyr::select(GEOID) |>
+	    sf::st_drop_geometry()) |>
+	  dplyr::pull(GEOID) |>
 	  unique()
 
 	# Counting disaster declarations by county by year and month
-	disaster_declarations <- disaster_declarations1 %>%
+	disaster_declarations <- disaster_declarations1 |>
 		dplyr::mutate(
 			fips_state_code = stringr::str_pad(as.character(fips_state_code), width = 2, side = "left", pad = "0"),
 			fips_county_code = stringr::str_pad(as.character(fips_county_code), width = 3, side = "left", pad = "0"),
 			GEOID = stringr::str_c(fips_state_code, fips_county_code),
 			year_declared = lubridate::year(declaration_date),
-			month_declared = lubridate::month(declaration_date)) %>%
+			month_declared = lubridate::month(declaration_date)) |>
 		## Major Disaster Declarations only
-		dplyr::filter(declaration_type == "DR") %>%
+		dplyr::filter(declaration_type == "DR") |>
 		## produce counts at the county x incident-type x year x month level
-		dplyr::group_by(GEOID, incident_type, year_declared, month_declared) %>%
+		dplyr::group_by(GEOID, incident_type, year_declared, month_declared) |>
 			dplyr::summarise(
 			  count = dplyr::n(),
-			  declaration_title = paste(declaration_title, collapse = ", ")) %>%
-			dplyr::ungroup() %>%
-	  dplyr::arrange(GEOID, year_declared, month_declared) %>%
+			  declaration_title = paste(declaration_title, collapse = ", ")) |>
+			dplyr::ungroup() |>
+	  dplyr::arrange(GEOID, year_declared, month_declared) |>
 		## widen the data so that there is one row per county x year x month, with
 		## incident-level counts in columns
-		tidyr::pivot_wider(names_from = incident_type, values_from = count, values_fill = list(count = 0), names_prefix = "incidents_") %>%
-	  dplyr::rename_with(.cols = dplyr::matches("incidents_"), janitor::make_clean_names) %>%
+		tidyr::pivot_wider(names_from = incident_type, values_from = count, values_fill = list(count = 0), names_prefix = "incidents_") |>
+	  dplyr::rename_with(.cols = dplyr::matches("incidents_"), janitor::make_clean_names) |>
 	  dplyr::mutate(
 			unique_id = uuid::UUIDgenerate(n = nrow(.)),
 			incidents_all = rowSums(dplyr::select(., dplyr::matches("incidents")), na.rm = TRUE),
-			incidents_natural_hazard = rowSums(dplyr::select(., dplyr::all_of(natural_hazards)), na.rm = TRUE)) %>%
-	  dplyr::rename_with(.cols = -GEOID, .fn = ~ .x %>% stringr::str_to_lower()) %>%
+			incidents_natural_hazard = rowSums(dplyr::select(., dplyr::all_of(natural_hazards)), na.rm = TRUE)) |>
+	  dplyr::rename_with(.cols = -GEOID, .fn = ~ .x |> stringr::str_to_lower()) |>
 	  dplyr::select(
 			unique_id,
 			GEOID,
@@ -96,15 +87,15 @@ get_fema_disaster_declarations_county = function(
 			declaration_title,
 			incidents_all,
 			incidents_natural_hazard,
-			dplyr::everything()) %>%
+			dplyr::everything()) |>
 	  dplyr::arrange(
 			GEOID,
 			year_declared,
-			month_declared) %>%
+			month_declared) |>
 	  dplyr::filter(GEOID %in% benchmark_geographies)
 
 	message(stringr::str_c(
-"The unit of observation is: ", geography, " x year x month. ",
+"The unit of observation is: county x year x month. ",
 "The `unique_id` field is a unique identifier for each observation. ",
 "Note that these data only describe Major Disaster Declarations."))
 
@@ -114,4 +105,5 @@ get_fema_disaster_declarations_county = function(
 utils::globalVariables(c(
   "get_box_data_path", "fips_state_code", "fips_county_code", "declaration_date",
   "declaration_type", "incident_type", "GEOID", "year_declared", "month_declared",
-  ".", "n", "count", "incidents_all", "incidents_natural_hazard", "benchmark_geographies"))
+  ".", "n", "count", "incidents_all", "incidents_natural_hazard", "benchmark_geographies",
+  "declaration_title"))
